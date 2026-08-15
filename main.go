8a18e9bb0231dc02810fbe5cf6e45efd238bc722
Flame Soulis <flamesoulis@flamesoulis.com>
2026-08-15 13:27:46 -0400

    Updated code to solution's

--- main.go
+++ main.go
@@ -1,19 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"bufio"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for true {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		inputedText := scanner.Text()
-		cleanedInput := cleanInput(inputedText)
-		fmt.Println("Your command was:", cleanedInput[0])
-	}
+	startRepl()
 }
 
--- repl.go
+++ repl.go
@@ -1,8 +1,28 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for true {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		cleanedInput := cleanInput(scanner.Text())
+		if len(cleanedInput) == 0 {
+			continue
+		}
+
+		commandName := cleanedInput[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
